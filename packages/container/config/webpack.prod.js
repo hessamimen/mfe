ac7a1efa7c97756cdd3b8a09b811426fd5fa976d
Hessam <hessam.imen@dijgtal.com>
2024-06-25 14:19:11 +1000

    feat: implement dashboard

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -17,6 +17,7 @@ const prodConfig = {
       remotes: {
         marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`, //the "marketing" key needs to match up the first part of some import statement inside of our container project.
         auth: `auth@${domain}/auth/latest/remoteEntry.js`,
+        dashboard: `dashboard@${domain}/dashboard/latest/remoteEntry.js`,
       },
       shared: packageJSON.dependencies,
     }),
